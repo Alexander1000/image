397437a8e2f8d68b51f6724d863d8087f072a3bd
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2016-07-09 23:44:37 +0300

    test create

--- test_create.sh
+++ test_create.sh
@@ -1,13 +1,9 @@
-./bin/binfilter ./images/derevo.jpg ./images/derevo.bin
-./bin/binfilter ./images/car.jpg ./images/car.bin
-./bin/binfilter ./images/profile.jpg ./images/profile.bin
-./bin/binfilter ./images/iphone.jpg ./images/iphone.bin
-./bin/binfilter ./images/iphone2.jpg ./images/iphone2.bin
-./bin/binfilter ./images/duhi.jpg ./images/duhi.bin
+images=(derevo car profile iphone iphone2 duhi)
 
-./bin/buildcostmap ./images/derevo.bin ./images/derevo.k
-./bin/buildcostmap ./images/car.bin ./images/car.k
-./bin/buildcostmap ./images/profile.bin ./images/profile.k
-./bin/buildcostmap ./images/iphone.bin ./images/iphone.k
-./bin/buildcostmap ./images/iphone2.bin ./images/iphone2.k
-./bin/buildcostmap ./images/duhi.bin ./images/duhi.k
+for ((i=0; i < ${#images[@]}; i++))
+do
+    echo "binfilter: " ${images[$i]}.jpg " => " ${images[$i]}.bin
+    ./bin/binfilter ./images/${images[$i]}.jpg ./images/${images[$i]}.bin
+    echo "buildcostmap: " ${images[$i]}.bin " => " ${images[$i]}.k
+    ./bin/buildcostmap ./images/${images[$i]}.bin ./images/${images[$i]}.k
+done
