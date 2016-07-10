0ece7a9d11fe6da4651da25e05feb058e4d429f8
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2016-07-10 11:01:01 +0300

    add parameter for create test

--- test_create.sh
+++ test_create.sh
@@ -6,10 +6,13 @@ do
     filename="${i##*/}"
     base="${filename%.[^.]*}"
 
-    echo "Found: " $base.jpg "..."
-    echo "binfilter: " $base.jpg " => " $base.bin
-    ./bin/binfilter ./images/$base.jpg ./images/$base.bin
-    echo "buildcostmap: " $base.bin " => " $base.k
-    ./bin/buildcostmap ./images/$base.bin ./images/$base.k
-    echo ""
+    if [[ $1 == "" || $1 == $base ]];
+    then
+        echo "Found: " $base.jpg "..."
+        echo "binfilter:" $base.jpg "=>" $base.bin
+        ./bin/binfilter ./images/$base.jpg ./images/$base.bin
+        echo "buildcostmap:" $base.bin "=>" $base.k
+        ./bin/buildcostmap ./images/$base.bin ./images/$base.k
+        echo ""
+    fi
 done
